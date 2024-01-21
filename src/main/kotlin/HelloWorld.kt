2379c15d09b4bbde2664f5d5cfb47130a59fbd0d
Ulises Cadena <ucadena07@gmail.com>
2024-01-21 07:57:39 -0600

    scopes

---
+++ src/main/kotlin/HelloWorld.kt
@@ -0,0 +1,27 @@
+package org.example
+
+import kotlinx.coroutines.*
+
+
+fun main(){
+    println("Program execution will now block")
+    runBlocking {
+        launch {
+            delay(1000L)
+            println("Task from runBlocking")
+        }
+
+        GlobalScope.launch {
+            delay(500L)
+            println("Task from GlobalScope")
+        }
+
+        coroutineScope {
+            launch {
+                delay(1500L)
+                println("Task from CoroutineScope")
+            }
+        }
+    }
+    println("Program execution will now continue")
+}
